40c2df5397c710e6142ca2be960c371281f95552
Mathieu Civel <mathieu.civel@gmail.com>
2018-11-27 08:23:17 +0100

    Fixed webpack dev mode removing dist folder

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,10 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('webapp-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
-    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Mathieu Civel',
       template: './src/index.html'
