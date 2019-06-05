bcc6e2d2508fdbeca4e40016ff02bdd0aafb7fa1
Mathieu Civel <mathieu.civel@gmail.com>
2019-06-05 11:17:49 +0200

    Updated dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,11 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new UglifyJSPlugin({
       sourceMap: false
     })
